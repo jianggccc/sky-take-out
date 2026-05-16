7655dae75795c45bff1b118c67c14f65ee0935d4
jianggc <jianggc.qq.com>
2026-05-16 18:30:29 +0800

    地址簿功能代码业务代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -15,7 +15,7 @@ public interface ShoppingCartMapper {
     List<ShoppingCart> list(ShoppingCart shoppingCart);
 
     @Update("update shopping_cart set number = #{number} where id = #{id}")
-    void update(ShoppingCart cartService);
+    void updateNumberById(ShoppingCart cartService);
 
     @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
             "values (#{name}, #{image}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
@@ -23,4 +23,7 @@ public interface ShoppingCartMapper {
 
     @Delete("delete from shopping_cart where user_id=#{userId}")
     void deleteByUserId(Long userId);
+
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
 }
